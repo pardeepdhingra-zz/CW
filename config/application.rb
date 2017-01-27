e5019333f793225bbf3c21320c3f2317627af4fb
Pardeep Dhingra <Pardeep.Dhingra@sial.com>
2017-01-27 14:15:01 +0530

    Skip test genration

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Cw
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.test_framework  nil #to skip test framework
+    end
   end
 end
